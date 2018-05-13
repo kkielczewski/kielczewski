4b785e866ad1330b9d08fb77c8e03e79ede1c5c2
Unknown <kam.kielczewski@gmail.com>
2018-05-13 14:16:34 +0200

    Whatever

---
+++ config/database.js
@@ -0,0 +1,26 @@
+module.exports = () => {
+  // Default to dev presets
+  const dbConfig = {
+    url: process.env.MONGODB_URI || "mongodb://localhost:27017/test",
+    opts: {
+      useMongoClient: true,
+      autoReconnect: true,
+      keepAlive: 300000,
+    },
+  };
+
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      break;
+    case 'stage':
+      break;
+    case 'test':
+      Object.assign(dbConfig, { url: process.env.MONGODB_URI || "mongodb://localhost:27017/test" });
+      break;
+    case 'dev':
+    default:
+      break;
+  }
+
+  return dbConfig;
+};
--- server.js
+++ server.js
@@ -1,6 +1,77 @@
+const Koa = require('koa');
+const mongoose = require('mongoose');
+const logger = require('koa-logger');
+const cors = require('kcors');
+const bodyParser = require('koa-bodyparser');
+const routes = require('./routes');
+const config = require('./config');
+
+// Make mongoose use native ES6 promises
+mongoose.Promise = global.Promise;
+
+// Connect to MongoDB
+mongoose.connect(config.database.url, config.database.opts);
+
+const app = new Koa()
+  .use(cors())
+  .use(logger())
+  .use(bodyParser())
+  .use(routes);
+
+const server = app.listen(config.server.port);
+
+module.exports = server;
+
+
+
+/*
+// Importing Node modules and initializing Express
+const express = require('express'),  
+      app = express(),
+      bodyParser = require('body-parser'),
+      logger = require('morgan'),
+      mongoose = require('mongoose'),
+      config = require('./config/main');
+      socketEvents = require('./socketEvents');
+const router = require('./router');
+
+
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test");
+
+// Start the server
+const server = app.listen(config.port, function () {
+  console.log('Your server is running on port ' + config.port + '.');
+});
+
+const io = require('socket.io').listen(server);
+
+socketEvents(io);  
+
+// Setting up basic middleware for all Express requests
+app.use(logger('dev')); // Log requests to API using morgan
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json()); 
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
+router(app); 
+
+
+*/
+/*
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
+var mongoose = require('mongoose');
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
@@ -12,25 +83,19 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/client/dist/";
 app.use(express.static(distDir));
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test").then(
+  () => { },
+  err => {
     console.log(err);
     process.exit(1);
   }
+);
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
 
 // CONTACTS API ROUTES BELOW
@@ -45,9 +110,9 @@ function handleError(res, reason, message, code) {
  *    GET: finds all contacts
  *    POST: creates a new contact
  */
-
+/*
 app.get("/api/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+  mongoose.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get contacts.");
     } else {
@@ -65,7 +130,7 @@ app.post("/api/contacts", function(req, res) {
     handleError(res, "Invalid user input", "Must provide a name.", 400);
   }
 
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+  mongoose.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to create new contact.");
     } else {
@@ -80,9 +145,9 @@ app.post("/api/contacts", function(req, res) {
  *    PUT: update contact by id
  *    DELETE: deletes contact by id
  */
-
+/*
 app.get("/api/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+  mongoose.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to get contact");
     } else {
@@ -95,7 +160,7 @@ app.put("/api/contacts/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  mongoose.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to update contact");
     } else {
@@ -106,7 +171,7 @@ app.put("/api/contacts/:id", function(req, res) {
 });
 
 app.delete("/api/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+  mongoose.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
     if (err) {
       handleError(res, err.message, "Failed to delete contact");
     } else {
@@ -114,3 +179,4 @@ app.delete("/api/contacts/:id", function(req, res) {
     }
   });
 });
+*/
