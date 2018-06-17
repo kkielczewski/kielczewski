feb0e2ef35270207ec7a35f96ab5a80aa047f787
Kamil <kam.kielczewski@gmail.com>
2018-06-17 22:00:06 +0200

    KC

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('kcors');
 const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
 const send = require('koa-send');
-const compress = require('koa-compress');
 const routes = require('./routes');
 const config = require('./config');
 const path = require('path');
@@ -17,24 +16,16 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.database.url, config.database.opts);
 
 // Koa
-/* eslint-disable */
 const app = new Koa()
   .use(cors())
   .use(logger())
   .use(bodyParser())
-  .use(compress({
-    filter: function (content_type) {
-       return /text/i.test(content_type)
-    },
-    threshold: 2048,
-    flush: require('zlib').Z_SYNC_FLUSH
-  }))
-  .use(serve(path.join(__dirname, '/client/dist')))
   .use(routes)
+  .use(serve(path.join(__dirname, '/client/dist')))
   .use(function* index() {
     yield send(this, '/client/dist/index.html');
   });
-/* eslint-enable */
+
 const server = app.listen(config.server.port);
 
 module.exports = server;
