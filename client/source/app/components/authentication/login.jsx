2bd1531c413bf68028060ea167f53220faac57a9
Kamil <kam.kielczewski@gmail.com>
2018-06-17 23:15:22 +0200

    s

--- client/source/app/components/authentication/login.jsx
+++ client/source/app/components/authentication/login.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
-import { translate, Trans } from 'react-i18next';
 import TextInput from '../form-fields/text-input';
 import GenericForm from '../form-fields/generic-form';
 import { login, CHANGE_AUTH } from '../../redux/modules/authentication';
@@ -24,6 +23,25 @@ class Login extends Component {
     loading: PropTypes.bool
   };
 
+  static formSpec = [
+    {
+      id: 'email',
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      placeholder: 'you@yourdomain.com',
+      component: TextInput
+    },
+    {
+      id: 'password',
+      name: 'password',
+      label: 'Password',
+      type: 'password',
+      placeholder: '********',
+      component: TextInput
+    }
+  ];
+
   handleFormSubmit = (formProps) => {
     const { desiredPath } = this.props;
     if (desiredPath) {
@@ -38,29 +56,8 @@ class Login extends Component {
       handleSubmit,
       errors,
       message,
-      loading,
-      t,
-      i18n
+      loading
     } = this.props;
-
-    const formSpec = [
-      {
-        id: 'email',
-        name: 'email',
-        label: 'Email',
-        type: 'email',
-        placeholder: 'you@yourdomain.com',
-        component: TextInput
-      },
-      {
-        id: 'password',
-        name: 'Password',
-        label: t('Password'),
-        type: 'password',
-        placeholder: '********',
-        component: TextInput
-      }
-    ];
 
     return (
       <div className={`auth-box ${loading ? 'is-loading' : ''}`}>
@@ -69,10 +66,10 @@ class Login extends Component {
           onSubmit={handleSubmit(this.handleFormSubmit)}
           errors={errors}
           message={message}
-          formSpec={formSpec}
-          submitText={t('Login')}
+          formSpec={Login.formSpec}
+          submitText="Login"
         />
-        <Link className="inline" to="/forgot-password">{t('Forgot password')}</Link> | <Link className="inline" to="/register">{t('Create account')}</Link>
+        <Link className="inline" to="/forgot-password">Forgot password?</Link> | <Link className="inline" to="/register">Create a new account.</Link>
       </div>
     );
   }
@@ -86,4 +83,4 @@ const mapStateToProps = ({ authentication }) => ({
   desiredPath: authentication.desiredPath
 });
 
-export default connect(mapStateToProps, { login })(translate('translations')(form(Login)));
+export default connect(mapStateToProps, { login })(form(Login));
